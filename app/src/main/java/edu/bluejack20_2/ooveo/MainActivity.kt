71eb5a44b8111633f2e7323b27ca927d1a801f94
nicholasleonardo <nicholas.trima@binus.ac.id>
2021-05-07 00:18:33 +0700

    Initial Commit

---
+++ app/src/main/java/edu/bluejack20_2/ooveo/MainActivity.kt
@@ -0,0 +1,11 @@
+package edu.bluejack20_2.ooveo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
