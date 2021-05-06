c229e2f8ed52961355f8d0eb6b985104baac0829
nicholasleonardo <nicholas.trima@binus.ac.id>
2021-05-07 00:22:11 +0700

    Leo Initial Commit

---
+++ app/src/main/java/edu/bluejack20_2/ooveo/HomeActivity.kt
@@ -0,0 +1,11 @@
+package edu.bluejack20_2.ooveo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
