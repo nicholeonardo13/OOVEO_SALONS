fb4f88326cb350b5a1436cb36560485682ceac1c
nicholasleonardo <nicholas.trima@binus.ac.id>
2021-05-07 23:00:25 +0700

    Leo Commit 2

--- app/src/main/java/edu/bluejack20_2/ooveo/MainActivity.kt
+++ app/src/main/java/edu/bluejack20_2/ooveo/MainActivity.kt
@@ -2,10 +2,27 @@ package edu.bluejack20_2.ooveo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val db = Firebase.firestore
+
+        val user = hashMapOf(
+            "first" to "Ada",
+            "last" to "Lovelace",
+            "born" to 1815
+        )
+
+// Add a new document with a generated ID
+        db.collection("users")
+            .add(user)
+
+        println(db.collection("tests").get())
+        println("TETETETETETETETETETETETE")
     }
 }
