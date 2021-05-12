7be1c60c17400ead26de1ede4b27416f5e71779b
nicholasleonardo <nicholas.trima@binus.ac.id>
2021-05-12 22:19:06 +0700

    Leo Commit 6

--- app/src/main/java/edu/bluejack20_2/ooveo/HomeActivity.kt
+++ app/src/main/java/edu/bluejack20_2/ooveo/HomeActivity.kt
@@ -5,20 +5,48 @@ import android.os.Bundle
 import android.util.Log
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
 import com.google.firebase.firestore.FirebaseFirestore
+import edu.bluejack20_2.ooveo.fragments.AppointmentFragment
+import edu.bluejack20_2.ooveo.fragments.FavouriteFragment
+import edu.bluejack20_2.ooveo.fragments.HomeFragment
+import edu.bluejack20_2.ooveo.fragments.ProfileFragment
+import edu.bluejack20_2.ooveo.fragments.adapters.viewPagerAdapter
 
 class HomeActivity : AppCompatActivity() {
 
-//    private lateinit var merchantRecyclerView: RecyclerView
+
     private val db = FirebaseFirestore.getInstance()
-//    private val collectionReference:CollectionReference = db.collection("test")
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
 //        readData()
+        setTab()
         getAllMerchantData()
+
+    }
+
+
+    fun setTab(){
+        val adapter = viewPagerAdapter(supportFragmentManager)
+        adapter.addFragment(HomeFragment() , "Home")
+        adapter.addFragment(FavouriteFragment() , "Favourite")
+        adapter.addFragment(AppointmentFragment(), "Appointment")
+        adapter.addFragment(ProfileFragment(), "Profile")
+        val viewPager = findViewById<ViewPager>(R.id.viewPager)
+        viewPager.adapter = adapter
+        val tabs = findViewById<TabLayout>(R.id.tabs)
+        tabs.setupWithViewPager(viewPager)
+
+        tabs.getTabAt(0)!!.setIcon(R.drawable.ic_baseline_home_24)
+        tabs.getTabAt(1)!!.setIcon(R.drawable.ic_baseline_favorite_24)
+        tabs.getTabAt(2)!!.setIcon(R.drawable.ic_baseline_calendar_today_24)
+        tabs.getTabAt(3)!!.setIcon(R.drawable.ic_baseline_account_circle_24)
+
     }
 
     fun readData(){
@@ -57,9 +85,9 @@ class HomeActivity : AppCompatActivity() {
                 }
 
                 var merchantAdapter = MerchantAdapter(listMerchantModel)
-                val recycleMerchants = findViewById<RecyclerView>(R.id.recycleMerchants)
-                   recycleMerchants.layoutManager = LinearLayoutManager(this)
-                    recycleMerchants.adapter = merchantAdapter
+//                val recycleMerchants = findViewById<RecyclerView>(R.id.recycleMerchants)
+//                   recycleMerchants.layoutManager = LinearLayoutManager(this)
+//                    recycleMerchants.adapter = merchantAdapter
 
             }
             .addOnFailureListener{
